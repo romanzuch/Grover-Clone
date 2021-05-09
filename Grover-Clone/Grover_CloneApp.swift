f047c40d1de38f51dfcc3227e08feba87380abd6
Roman <roman.zuchowski@gmail.com>
2021-05-09 13:36:58 +0200

    Initial Commit

---
+++ Grover-Clone/Grover_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Grover_CloneApp.swift
+//  Grover-Clone
+//
+//  Created by Roman on 09.05.21.
+//
+
+import SwiftUI
+
+@main
+struct Grover_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
