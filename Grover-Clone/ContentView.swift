bcb5dc1aaff50b915ff0e929a2ab18d99f782c68
Roman <roman.zuchowski@gmail.com>
2021-05-09 17:57:05 +0200

    viewRouter is added

--- Grover-Clone/ContentView.swift
+++ Grover-Clone/ContentView.swift
@@ -8,8 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    // MARK: - state proerties for showing different views
+    @StateObject var viewRouter: ViewRouter
+    @State private var showMenu: Bool = false
+    @State private var showShoppingBag: Bool = false
+    
+    // MARK: - state properties for managing user search queries
+    @State private var searchTerm: String = ""
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        GeometryReader { geometry in
+            VStack {
+                
+                HeaderView(geometry: geometry, viewRouter: viewRouter, searchTerm: $searchTerm)
+                
+                Spacer()
+                
+                if self.searchTerm != "" {
+                    SearchResults()
+                } else {
+                    switch viewRouter.currentPage {
+                    case .home:
+                        HomeView(geometry: geometry)
+                    case .menu:
+                        MenuView()
+                    case .shoppingBag:
+                        ShoppingBagView()
+                    case .account:
+                        AccountView()
+                    }
+                }
+                
+                Spacer()
+                
+                FooterView(viewRouter: viewRouter, geometry: geometry)
+                    .frame(width: geometry.size.width, height: geometry.size.height/10)
+            }
+        }
+        
     }
 }
