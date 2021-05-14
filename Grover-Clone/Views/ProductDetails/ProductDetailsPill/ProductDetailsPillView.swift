ea12f7e2836013121f527cdc41915592b760ddf1
Roman <roman.zuchowski@gmail.com>
2021-05-14 09:50:35 +0200

    changed property names

--- Grover-Clone/Views/ProductDetails/ProductDetailsPill/ProductDetailsPillView.swift
+++ Grover-Clone/Views/ProductDetails/ProductDetailsPill/ProductDetailsPillView.swift
@@ -11,16 +11,16 @@ struct ProductDetailsPillView: View {
     
     var geometry: GeometryProxy
     var title: String
-    var subtitle: String
+    var description: String
     var deal: Bool
     
     init(geometry: GeometryProxy,
          title: String,
-         subtitle: String,
+         description: String,
          deal: Bool) {
         self.geometry = geometry
         self.title = title
-        self.subtitle = subtitle
+        self.description = description
         self.deal = deal
     }
     
@@ -44,10 +44,27 @@ struct ProductDetailsPillView: View {
                         .padding(2)
                 }
                 .foregroundColor(Color("GroverGrey"))
+                .padding(.trailing, 4)
             }
-            Text(title)
-                .font(.title2).fontWeight(.bold)
-                .foregroundColor(Color("GroverGrey"))
+            
+            Group {
+                Text(title)
+                    .font(.title2).fontWeight(.bold)
+                    .foregroundColor(Color("GroverGrey"))
+                Text(description)
+                    .padding(.vertical, 8)
+            }
+            
+            Group {
+                Label("GRATIS Grover Care", systemImage: "umbrella")
+                Label("Lieferung in 1-3 Werktagen", systemImage: "bus")
+            }
+            
+            Text("Wähle eine Mindestmietdauer")
         }
+        .padding()
+        .background(Color.white)
+        .clipShape(RoundedRectangle(cornerRadius: 6.0, style: .continuous))
+        .shadow(color: .black.opacity(0.25), radius: 10, x: 0.0, y: 0.0)
     }
 }
