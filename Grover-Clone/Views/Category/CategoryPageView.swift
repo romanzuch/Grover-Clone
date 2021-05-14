60c179f344b8e0d1a7fbe1012e9fd10969eeabb9
Roman <roman.zuchowski@gmail.com>
2021-05-14 09:49:08 +0200

    Initial Commit

---
+++ Grover-Clone/Views/Category/CategoryPageView.swift
@@ -0,0 +1,20 @@
+//
+//  CategoryPageView.swift
+//  Grover-Clone
+//
+//  Created by Roman on 14.05.21.
+//
+
+import SwiftUI
+
+struct CategoryPageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CategoryPageView_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryPageView()
+    }
+}
