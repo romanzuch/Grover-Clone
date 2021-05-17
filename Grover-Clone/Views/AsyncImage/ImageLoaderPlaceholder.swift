5980c10211d8dc61db3f06b7b413a087885ad8d8
Roman <roman.zuchowski@gmail.com>
2021-05-17 10:36:35 +0200

    default circular progress view atm

---
+++ Grover-Clone/Views/AsyncImage/ImageLoaderPlaceholder.swift
@@ -0,0 +1,14 @@
+//
+//  ImageLoaderPlaceholder.swift
+//  Grover-Clone
+//
+//  Created by Roman on 17.05.21.
+//
+
+import SwiftUI
+
+struct ImageLoaderPlaceholder: View {
+    var body: some View {
+        ProgressView().progressViewStyle(CircularProgressViewStyle())
+    }
+}
