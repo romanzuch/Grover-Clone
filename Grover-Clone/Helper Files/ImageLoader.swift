01b7fbe0462a8fb60d562d036db84a7ea0a1abb5
Roman <roman.zuchowski@gmail.com>
2021-05-17 10:35:52 +0200

    loads image from provided url

--- Grover-Clone/Helper Files/ImageLoader.swift
+++ Grover-Clone/Helper Files/ImageLoader.swift
@@ -10,5 +10,28 @@ import Combine
 import Foundation
 
 class ImageLoader: ObservableObject {
+    @Published var image: UIImage?
+    private let url: URL
     
+    init(url: URL) {
+        self.url = url
+    }
+    
+    deinit {
+        cancel()
+    }
+    
+    private var cancellable: AnyCancellable?
+    
+    func load() {
+        cancellable = URLSession.shared.dataTaskPublisher(for: url)
+            .map { UIImage(data: $0.data) }
+            .replaceError(with: nil)
+            .receive(on: DispatchQueue.main)
+            .sink { [weak self] in self?.image = $0 }
+    }
+    
+    func cancel() {
+        cancellable?.cancel()
+    }
 }
