135f177e780155435bb1feb564d0ca2a374dbebf
Roman <roman.zuchowski@gmail.com>
2021-05-26 17:45:13 +0200

    the image loader now accesses the image cache

--- Grover-Clone/Helper Files/ImageLoader.swift
+++ Grover-Clone/Helper Files/ImageLoader.swift
@@ -12,9 +12,11 @@ import Foundation
 class ImageLoader: ObservableObject {
     @Published var image: UIImage?
     private let url: URL
+    private var cache: ImageCache?
     
-    init(url: URL) {
+    init(url: URL, cache: ImageCache? = nil) {
         self.url = url
+        self.cache = cache
     }
     
     deinit {
@@ -24,11 +26,20 @@ class ImageLoader: ObservableObject {
     private var cancellable: AnyCancellable?
     
     func load() {
+        if let image = cache?[url] {
+            self.image = image
+            return
+        }
+        
         cancellable = URLSession.shared.dataTaskPublisher(for: url)
             .map { UIImage(data: $0.data) }
             .replaceError(with: nil)
             .receive(on: DispatchQueue.main)
             .sink { [weak self] in self?.image = $0 }
+    }
+    
+    private func cache(_ image: UIImage?) {
+        image.map { cache?[url] = $0 }
     }
     
     func cancel() {
