aacada60b95d70e383bb82f7e6bc4d95c696804b
Roman <roman.zuchowski@gmail.com>
2021-05-27 20:45:20 +0200

    now shows the months instead of just the index; it is rudimentary though as it doesn't adapt to different data formats

--- Grover-Clone/Views/ProductDetails/ProductDetailsPill/RentingDurationPicker.swift
+++ Grover-Clone/Views/ProductDetails/ProductDetailsPill/RentingDurationPicker.swift
@@ -20,8 +20,19 @@ struct RentingDurationPicker: View {
     var body: some View {
         HStack {
             Picker("", selection: $selectedDuration) {
-                ForEach(0..<prices.count, id: \.self) { price in
-                    Text("\(price)")
+                ForEach((0..<prices.count).reversed(), id: \.self) { index in
+                    switch index {
+                    case 0:
+                        Text("12")
+                    case 1:
+                        Text("6")
+                    case 2:
+                        Text("3")
+                    case 3:
+                        Text("1")
+                    default:
+                        Text("1")
+                    }
                 }
             }
             .pickerStyle(SegmentedPickerStyle())
